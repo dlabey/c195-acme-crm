787e12cdbc726fb6302bb4d5741da78df8df1ba5
Darren Labey <darren@Darrens-iMac.local>
2017-03-24 21:23:32 -0700

    Proper setup

---
+++ src/main/java/com/acme/crm/controllers/LoginController.java
@@ -0,0 +1,30 @@
+package com.acme.crm.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author darren
+ */
+public class LoginController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
